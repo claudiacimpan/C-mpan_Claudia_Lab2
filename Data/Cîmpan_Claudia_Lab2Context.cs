9b08fc7d56bacaf74110dc4bce93457aea51fc83
Claudia Cîmpan <campan.claudia77@gmail.com>
2022-10-26 15:56:06 +0300

    Add project files.

---
+++ Data/Cîmpan_Claudia_Lab2Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Cîmpan_Claudia_Lab2.Models;
+
+namespace Cîmpan_Claudia_Lab2.Data
+{
+    public class Cîmpan_Claudia_Lab2Context : DbContext
+    {
+        public Cîmpan_Claudia_Lab2Context (DbContextOptions<Cîmpan_Claudia_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Cîmpan_Claudia_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Cîmpan_Claudia_Lab2.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Cîmpan_Claudia_Lab2.Models.Author> Author { get; set; }
+    }
+}
---
+++ Models/Author.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cîmpan_Claudia_Lab2.Models
+{
+    public class Author
+    {
+        public int ID { get; set; }
+        [Display (Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        public ICollection<Book>? Books { get; set; }
+    }
+}
