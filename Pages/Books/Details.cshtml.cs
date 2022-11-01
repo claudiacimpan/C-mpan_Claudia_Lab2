258208f800988356c93c4ba829b4a49494aed83c
Claudia Cîmpan <campan.claudia77@gmail.com>
2022-11-01 16:25:32 +0200

    details-update

--- Pages/Books/Details.cshtml.cs
+++ Pages/Books/Details.cshtml.cs
@@ -29,7 +29,7 @@ namespace Cîmpan_Claudia_Lab2.Pages.Books
                 return NotFound();
             }
 
-            var book = await _context.Book.FirstOrDefaultAsync(m => m.ID == id);
+            var book = await _context.Book.Include(i=>i.Author).FirstOrDefaultAsync(m => m.ID == id);
             if (book == null)
             {
                 return NotFound();
