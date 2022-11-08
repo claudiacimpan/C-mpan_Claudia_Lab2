2483883684cb30a40a624c3c5f3db36435946347
Claudia Cîmpan <campan.claudia77@gmail.com>
2022-11-08 14:43:00 +0200

    Lab4-complet

--- Pages/Categories/Index.cshtml.cs
+++ Pages/Categories/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Cîmpan_Claudia_Lab2.Data;
 using Cîmpan_Claudia_Lab2.Models;
+using Cîmpan_Claudia_Lab2.Models.ViewModels;
 
 namespace Cîmpan_Claudia_Lab2.Pages.Categories
 {
@@ -19,14 +20,54 @@ namespace Cîmpan_Claudia_Lab2.Pages.Categories
             _context = context;
         }
 
-        public IList<Category> Category { get;set; } = default!;
-
-        public async Task OnGetAsync()
+        public IList<Category> Category { get; set; } = default!;
+        public CategoriesIndexData CategoryData { get; set; }
+        public int CategoryID { get; set; }
+        public int BookID { get; set; }
+        public async Task OnGetAsync(int? id, int? bookID)
         {
-            if (_context.Category != null)
+            CategoryData = new CategoriesIndexData();
+            CategoryData.Categories = await _context.Category
+                .Include(i => i.BookCategories)
+                .ThenInclude(i => i.Book)
+                .ThenInclude(c => c.Author)
+                .OrderBy(i => i.CategoryName)
+                .ToListAsync();
+            if (id != null)
             {
-                Category = await _context.Category.ToListAsync();
+                CategoryID = id.Value;
+                Category category = CategoryData.Categories
+                .Where(i => i.ID == id.Value).Single();
+                CategoryData.BookCategories = category.BookCategories;
             }
         }
+        /* public async Task OnGetAsync()
+         {
+             if (_context.Category != null)
+             {
+                 Category = await _context.Category.ToListAsync();
+             }
+         }*/
+        /* public PublisherIndexData PublisherData { get; set; }
+         public int PublisherID { get; set; }
+         public int BookID { get; set; }
+         public async Task OnGetAsync(int? id, int? bookID)
+         {
+             PublisherData = new PublisherIndexData();
+             PublisherData.Publishers = await _context.Publisher
+             .Include(i => i.Books)
+             .ThenInclude(c => c.Author)
+             .OrderBy(i => i.PublisherName)
+             .ToListAsync();
+             if (id != null)
+             {
+                 PublisherID = id.Value;
+                 Publisher publisher = PublisherData.Publishers
+                 .Where(i => i.ID == id.Value).Single();
+                 PublisherData.Books = publisher.Books;
+             }
+
+         }
+     }*/
     }
-}
+    }
